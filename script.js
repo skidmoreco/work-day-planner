0767bfd0c39e79a1ad9154fe389e144421118b19
Corey Skidmore <skidmoreeco@gmail.com>
2022-08-15 14:50:27 -0400

    final commit

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-var todayDateEl = $("#currentDay")
+var todayDateEl = $('#currentDay');
+var formGroups = $('.form-group');
+var saveBtn = $('.saveBtn');
+var formContent = $('.form-control')
 
+function saveInformation(index) {
+    var newData = formContent[index].value;
+    localStorage.setItem("data" + index, newData);
+}
 
-
-
+for (let i = 0; i < formGroups.length; i++) {
+    var data = localStorage.getItem("data" + i);
+    if (data == undefined) {
+        localStorage.setItem("data" + i, " ");
+    } else if (data != " ") {
+        formContent[i].value = data
+    }
+    saveBtn[i].addEventListener("click", function(){
+        saveInformation(i);
+    })
+}
 
 
 
 function displayDate() {
-var rightNow = moment().format("MMMM Do, YYYY")
-todayDateEl.text(rightNow);
+var rightNow = moment().format("MMMM Do, YYYY hh:mm:ss")
+todayDateEl.text(rightNow + " PM");
 }
 
 
